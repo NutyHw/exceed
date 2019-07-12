4a404ed7e857ab5047652011a6e0309eb1417558
Nuttupoom <nuty@Nuttupooms-MacBook-Air.local>
2019-07-13 00:23:58 +0700

    bug fix graph.js

--- graph.js
+++ graph.js
@@ -1,51 +1,39 @@
 const fetch = require("node-fetch");
-
 var url = "https://exceed.superposition.pknn.dev/data/boobo_hardware";
 var db_url = "https://exceed.superposition.pknn.dev/data/boobo_db";
-function data_transfer(watch_id) {
-    fetch(url)
-    .then(function(res) {
-        return res.json();
-    })
-    .then(function(myjson) {
-        if (myjson["isWear"] == "False") {
-            console.log('took watch off')
-            fetch(db_url)
-            .then(function(res){
-                return res.json()
-            })
-            .then(function(res){
-                var date = new Date().toLocaleString()
-                res[watch_id][date] = 'took off'
-                fetch(db_url,{
-                    method: "POST",
-                    body: JSON.stringify({ data: res }),
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                })
-            })
-        } 
-        else if (myjson["isDanger"] == "True") {
-            console.log('in Danger')
-            fetch(db_url)
-                .then(function (res) {
-                    return res.json()
-                })
-                .then(function (res) {
-                    var date = new Date().toLocaleString()
-                    res[watch_id][date] = 'in Danger'
-                    fetch(db_url, {
-                        method: "POST",
-                        body: JSON.stringify({ "data": res }),
-                        headers: {
-                            "Content-Type": "application/json"
-                        }
-                    }) 
-                })
-            }
-        })
-    }
+async function data_transfer(watch_id) {
+    res = await fetch(url)
+    EVENT = await res.json()
+    res = await fetch(db_url)
+    data = await res.json()
 
-data_transfer(345)
+    if(EVENT['isWear'] == "False"){
+        var date = new Date().toLocaleString()
+        data[watch_id][date] = 'took off'
+        fetch(db_url,{
+            method : "POST",
+            headers : {
+                "Content-Type" : "application/json"
+            },
+            body : JSON.stringify({"data" : data})
+        })
+        .then(res => console.log('success'))
+        .catch(err => console.log('Something occure'))
+    }
+    else if(EVENT['isDanger'] == 'True'){
+        var date = new Date().toLocaleString()
+        data[watch_id][date] = 'in Danger'
+        fetch(db_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ "data": data })
+        })
+            .then(res => console.log('success'))
+            .catch(err => console.log('Something occure')) 
+    }
+}
+
+
 
